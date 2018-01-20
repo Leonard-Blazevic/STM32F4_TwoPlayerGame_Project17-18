dd23a71c4704c892ce8d02dcb86679e9b45f994c
Tin Raic <tin.raic@fer.hr>
2018-01-21 00:25:19 +0100

    Added programs running on ESP's, AP-acces point, STA-station

---
+++ Project/Firmware/AP_UDP.ino
@@ -0,0 +1,110 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#define NAME "ESP_01_AP"
+#define PASSWORD "test12586"
+#define PACKET_SIZE 100
+#define STOP_SIGN '\n'
+
+IPAddress staticIP(192,168,1,15);        //IP configuration for AP
+IPAddress gateway(192,168,1,22);
+IPAddress subnet(255,255,255,0);
+
+IPAddress stationStaticIP(192,168,1,22); //IP and port configuration data from STATION (CLIENT)
+unsigned int stationRemotePort = 4210;
+
+WiFiUDP Udp;
+unsigned int localUdpPort = 4281;        //arbitrary values for port and array size
+char receivePacket[PACKET_SIZE];                   
+char  transmitPacket[PACKET_SIZE]; 
+
+void setup() {
+
+  WiFi.mode(WIFI_AP);
+  
+  Serial.begin(115200);                  //starting serial communication, baudrate 115200 (ESP default)
+
+  while(1){                                              //setting AP
+    boolean result = WiFi.softAP(NAME, PASSWORD);
+    if(result)
+    {
+      boolean configChange = WiFi.softAPConfig(staticIP, gateway, subnet);
+      if(configChange){
+        break;
+      }
+    }
+    else
+    {
+      Serial.println("AP not created! Trying again...");
+      delay(2000);
+    }
+  }
+
+  Serial.println("Waiting for player 2 to join...");
+  
+  while(WiFi.softAPgetStationNum() != 1)         //waiting for station (client) to connect
+  {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("Player has joined the game!");
+
+  Udp.begin(localUdpPort);
+
+}
+
+void loop() {
+
+  int packetSize = Udp.parsePacket();             //receiving incoming UDP packets
+  if (packetSize)
+  {
+    int len = Udp.read(receivePacket, PACKET_SIZE);
+    if (len > 0)
+    {
+      receivePacket[len] = 0;
+    }
+    Serial.print(receivePacket);
+    memset(receivePacket, 0, sizeof(receivePacket));     //resetting receive buffer
+    
+  }
+  
+  if (receiveUntilStopSign() >= 1) 
+  {
+    Udp.beginPacket(stationStaticIP, stationRemotePort);
+    Udp.write(transmitPacket);
+    Udp.endPacket();
+    memset(transmitPacket, 0, sizeof(transmitPacket));  //resetting transmit values
+   
+  }
+
+}
+
+int receiveUntilStopSign(){
+  char receivedByte;
+  int position = 0;
+  int numOfReceivedBytes = 0;
+  
+  while(Serial.available() > 0)
+  {
+    receivedByte = Serial.read();
+
+    if(receivedByte != STOP_SIGN)
+    {
+     transmitPacket[position++] = receivedByte;
+     numOfReceivedBytes++;
+
+     if(position >= PACKET_SIZE)
+     {
+      position = 0;
+     }
+    }
+    else 
+    {
+      transmitPacket[position] = '\0';
+      return numOfReceivedBytes;
+    }
+  }
+  return 0;
+  
+}
+
---
+++ Project/Firmware/STA_UDP.ino
@@ -0,0 +1,92 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#define NAME "ESP_01_AP"
+#define PASSWORD "test12586"
+#define PACKET_SIZE 100
+#define STOP_SIGN '\n'
+
+IPAddress staticIP(192,168,1,22);   //IP configuration for STATION (CLIENT)
+IPAddress gateway(192,168,1,15);
+IPAddress subnet(255,255,255,0);
+
+IPAddress apStaticIP(192,168,1,15); //IP and port configuration data from AP
+unsigned int apRemotePort = 4281;
+
+WiFiUDP Udp;
+unsigned int localUdpPort = 4210;  //arbitrary values
+char receivePacket[PACKET_SIZE];           
+char  transmitPacket[PACKET_SIZE];
+         
+void setup() {
+
+  WiFi.mode(WIFI_STA);
+  
+  Serial.begin(115200);
+
+  Serial.println("Joining game...");
+  WiFi.begin(NAME, PASSWORD);
+  boolean configChange = WiFi.config(staticIP, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED && !configChange)
+  {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println("Joined game!");
+
+  Udp.begin(localUdpPort);
+
+}
+
+void loop() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    // receive incoming UDP packets
+    int len = Udp.read(receivePacket, PACKET_SIZE);
+    if (len > 0)
+    {
+      receivePacket[len] = 0;
+    }
+    Serial.print(receivePacket);
+    memset(receivePacket, 0, sizeof(receivePacket));                //resetting receive buffer
+    
+  }
+    
+  if (receiveUntilStopSign() >= 1) {
+    Udp.beginPacket(apStaticIP, apRemotePort);
+    Udp.write(transmitPacket);
+    Udp.endPacket();
+    memset(transmitPacket, 0, sizeof(transmitPacket));             //resetting transmit values
+   
+  }
+
+}
+
+int receiveUntilStopSign(){
+  char receivedByte;
+  int position = 0;
+  int numOfReceivedBytes = 0;
+  
+  while(Serial.available() > 0)
+  {
+    receivedByte = Serial.read();
+
+    if(receivedByte != STOP_SIGN)
+    {
+     transmitPacket[position++] = receivedByte;
+     numOfReceivedBytes++;
+
+     if(position >= PACKET_SIZE)
+     {
+      position = 0;
+     }
+    }
+    else 
+    {
+      transmitPacket[position] = '\0';
+      return numOfReceivedBytes;
+    }
+  }
+  return 0;
+  
+}
